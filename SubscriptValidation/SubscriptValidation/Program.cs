e0eba5e5172f98ca8eacf65fb83e8dd1ce408edd
Wilman Gonzales <wilmangonzales@rocketmail.com>
2016-08-05 09:16:47 -0400

    Add files via upload

---
+++ SubscriptValidation/SubscriptValidation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Chapter 11 exercise 1
+// Wilman Gonzales
+
+
+namespace SubscriptValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int[] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Please enter a number from 1 to 10");
+
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                
+                   if (number < 1 || number > 10) throw new IndexOutOfRangeException();
+                Console.WriteLine("Your number is {0}", number);
+                Console.ReadLine();
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Your number is out of range, Please enter a number from 1 to 10");
+                Console.ReadLine();
+            }
+        }
+    }
+}
