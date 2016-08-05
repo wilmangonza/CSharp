e12ed69c44e89810edd9084721c29ef3d68ecf17
Wilman Gonzales <wilmangonzales@rocketmail.com>
2016-08-05 08:36:15 -0400

    Add files via upload

---
+++ Planets/Planets/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Planets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Display Name of Planet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the Number of the Planet";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(84, 77);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(116, 20);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Planet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+    }
+}
+
---
+++ Planets/Planets/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Chapter 2 Exercise 16
+//Developer Wilman Gonzales
+
+namespace Planets
+{
+    public partial class Form1 : Form
+
+    {
+        // setting the enumeration
+        enum Planet
+        {
+           Mercury = 1,
+           Venus = 2,
+           Earth = 3,
+           Mars = 4,
+           Jupiter = 5,
+           Saturn = 6,
+           Uranus= 7,
+           Neptune = 8,
+            
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int planetNumber;
+            int.TryParse(inputTextBox.Text, out planetNumber);
+            if (planetNumber < 1 || planetNumber > 8)
+            {
+                MessageBox.Show("Please enter an integer between 1 and 8");
+                inputTextBox.Focus();
+                return;
+            }
+            string planetName = ((Planet)planetNumber).ToString();
+            MessageBox.Show("You entered the planet number for " + planetName);
+            inputTextBox.Clear();
+            inputTextBox.Focus();
+        }
+    }
+}
